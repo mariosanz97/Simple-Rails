81cf1391a77ce9b93002afc4c1ca2b4d83ae91af
mariosanz97 <mario.s_97@hotmail.com>
2018-08-11 16:29:30 +0200

    Sass styles added in admin panel
    -Bootstrap gem isntalled
    -Sass, variables and mixins added on admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'admin/index', to: 'admin#index'
   get 'admin/show'
-  get 'admin/create'
+  get 'admin/create', to: 'admin#create'
   get 'admin/edit'
   get 'admin/update'
   get 'admin/destroy'
