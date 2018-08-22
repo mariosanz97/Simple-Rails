dd0bad28bd643552a9a0880bada7ae84baa0864c
mariosanz97 <mario.s_97@hotmail.com>
2018-08-23 01:02:13 +0200

    Set env, use link_to with class

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 #GET
     get 'admin/index', to: 'admin#index'
     get 'admin/:id/edit', to: 'admin#edit', as: 'edit_admin'
-    get 'admin/destroy'
+    get 'admin/destroy', to: 'admin#destroy', as: 'delete_admin'
     #create post 
     post 'admin', to: 'admin#create'
     #edit put
