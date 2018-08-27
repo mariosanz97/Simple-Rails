a4603a29b4eef02f614c6ceef87cd5c47acc5126
mariosanz97 <mario.s_97@hotmail.com>
2018-08-27 21:35:29 +0200

    Create Login and auth with Devise -Intall gem devise -Create user schema and migration -Requiere auth -No sing_up permision -Giving style with bootrstrap -Media queries

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'admin#index', as: 'admin_index'
+  devise_for :users
+
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new", as: "login"
+  end
+
 #GET
     #admin
-    get 'admin/index', to: 'admin#index', as: 'admin'
+    #get 'admin/index', to: 'admin#index', as: 'admin'
     get 'admin/:id/edit', to: 'admin#edit', as: 'edit_admin'
     get 'admin/:id/destroy', to: 'admin#destroy', as: 'delete_admin'
     #create post 
